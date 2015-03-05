23abb5c470b25513af21edab2eb2c853450d92f4
Michal Špaček <tupinek@gmail.com>
2015-03-05 16:40:29 +0100

    Prepare for 0.05 version.

--- Bucharest.pm
+++ Bucharest.pm
@@ -11,7 +11,7 @@ use Moo;
 use namespace::clean;
 
 # Version.
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Get XML.
 has xml => (
@@ -244,6 +244,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
