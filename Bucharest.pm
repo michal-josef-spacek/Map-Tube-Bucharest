41f99f1e04067925392c7829f25348f89cb86887
Michal Špaček <tupinek@gmail.com>
2015-10-04 10:07:55 +0200

    Add EXAMPLE4 to doc.

--- Bucharest.pm
+++ Bucharest.pm
@@ -197,6 +197,31 @@ For more information about Bucharest Map, click L<here|https://en.wikipedia.org/
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Bucharest;
+
+ # Object.
+ my $obj = Map::Tube::Bucharest->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Linia M1
+ # Linia M2
+ # Linia M3
+ # Linia M4
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
